e5b6424e1b6dcf65c35b5c8d1fde72ea01565d36
c19 <classone2010@gmail.com>
2014-01-10 14:28:14 +0800

    interface improvement

--- C_term_project/minibase.c
+++ C_term_project/minibase.c
@@ -27,10 +27,12 @@ typedef enum PARSE_LINE_ERROR_FLAG
 #define MAX_KEYS_IN_NODE 1024
 #define MAX_POINTERS_IN_NODE (MAX_KEYS_IN_NODE + 1)
 #define MAX_RECORDS_IN_LEAF 64
+#define u_int unsigned int
+
 typedef struct Node
 {
 	char   **keys;       //keys in current Node, i use char *as key.
-	size_t key_num;    //the number of keys/records in current Node
+	u_int key_num;    //the number of keys/records in current Node
 	struct Node  *parent;     //parent Node
 	void   **pointers;   //pointers to children nodes if it's a Node,pointers to Records if it's a leaf Node.
 	bool   is_leaf;
@@ -38,17 +40,18 @@ typedef struct Node
 } Node;
 
 // character which separate the key and value
-#define SEPARATER ' '
+
 typedef struct Record
 {
 	char *key;
 	char *value;
 } Record;
-
 #include "minibase.h"
 
+char SEPATATOR = ',';
+
 void print_record(const Record *record){
-	printf("record:%s%c%s\n", record->key, SEPARATER, record->value);
+	printf("record:%s%c%s\n", record->key, SEPATATOR, record->value);
 }
 
 Record *make_record(char *key, char *value) {
@@ -74,7 +77,7 @@ Node *make_leaf(void){
 	return leaf;
 }
 
-Node *_make_node(size_t max_key_num, bool is_leaf) {
+Node *_make_node(u_int max_key_num, bool is_leaf) {
 	Node *new_node;
 	new_node = (Node *)malloc(sizeof(Node));
 	// leaf_node have MAX_RECORDS_IN_LEAF keys and pointers
@@ -91,24 +94,24 @@ Node *_make_node(size_t max_key_num, bool is_leaf) {
 // like memcpy, but do it backward. for the dst and src are overlapping.
 // ====src++++dst____
 // ====src====src++++		// result like this.
-void shift_backward(void *dst, const void *src, size_t length){
+void shift_backward(void *dst, const void *src, u_int length){
 	while(length--)
 		*((char *)dst + length) = *((char *)src + length);
 }
 
-void pointer_shift(void **dst, const void **src, size_t length){
+void pointer_shift(void **dst, const void **src, u_int length){
 	while(length--)
 		*((char **)dst + length) = *((char **)src + length);
 }
 
-void pointer_copy(void **dst, const void **src, size_t length){
+void pointer_copy(void **dst, const void **src, u_int length){
 	while(length--)
 		*(char **)dst++ = *(char **)src++;
 }
 
 // =.= , term project require us not to use string.h.
 // what a bad idea, isn't it.
-void memcpy(void *dst, const void *src, size_t length){
+void memcpy(void *dst, const void *src, u_int length){
 	while(length--)
 		*(char *)dst++ = *(char *)src++;
 }
@@ -150,7 +153,7 @@ int b_search_subtree(void *base, int total, char *target, int(*cmp)(char const *
 
 // return whether have a exact match 
 // index of exact match subscript or 
-bool b_search_key(size_t *index, void *base, size_t total, char *target, int(*cmp)(char const *, char const *)){
+bool b_search_key(u_int *index, void *base, u_int total, char *target, int(*cmp)(char const *, char const *)){
 	int head = 0;
 	int tail = total - 1;
 	int mid, result;
@@ -184,7 +187,7 @@ Node *find_leaf(Node *root, char *key) {
 //return 0 if not found
 Record *find(Node *root, char *key) {
 	Node *leaf = find_leaf(root, key);
-	size_t index;
+	u_int index;
 	if (b_search_key(&index, leaf->keys, leaf->key_num, key, strcmp))
 	{
 		return (Record *)leaf->pointers[index];
@@ -238,24 +241,24 @@ Node *insert_record(Node *root, Record *new_record) {
 	// return insert_into_leaf_and_split(root, leaf, new_record->key, new_record);
 }
 
-void _write_into_leaf(Node *leaf, char *key, size_t insertion_point, void *record){
+void _write_into_leaf(Node *leaf, char *key, u_int insertion_point, void *record){
 	leaf->keys[insertion_point] = key;
 	leaf->pointers[insertion_point] = record;
 }
 
-void _add_into_leaf(Node *leaf, char *key, size_t insertion_point, void *record){
+void _add_into_leaf(Node *leaf, char *key, u_int insertion_point, void *record){
 	leaf->keys[insertion_point] = key;
 	leaf->pointers[insertion_point] = record;
 	leaf->key_num++;
 }
 
-void _write_into_node(Node *node, char *key, size_t insertion_point, void *left_node, void *right_node){
+void _write_into_node(Node *node, char *key, u_int insertion_point, void *left_node, void *right_node){
 	node->keys[insertion_point] = key;
 	node->pointers[insertion_point] = left_node;
 	node->pointers[insertion_point + 1] = right_node;
 }
 
-void _add_into_node(Node *node, char *key, size_t insertion_point, void *left_node, void *right_node){
+void _add_into_node(Node *node, char *key, u_int insertion_point, void *left_node, void *right_node){
 	node->keys[insertion_point] = key;
 	node->pointers[insertion_point] = left_node;
 	node->pointers[insertion_point + 1] = right_node;
@@ -263,7 +266,7 @@ void _add_into_node(Node *node, char *key, size_t insertion_point, void *left_no
 }
 
 Node *insert_into_leaf(Node *root, Node *leaf, char *key, Record *pointer) {
-	size_t insertion_point;
+	u_int insertion_point;
 	//no exact match
 	if (!b_search_key(&insertion_point, leaf->keys, leaf->key_num, key, strcmp)){
 		if (leaf->key_num == MAX_RECORDS_IN_LEAF){ // Split needed.
@@ -280,7 +283,7 @@ Node *insert_into_leaf(Node *root, Node *leaf, char *key, Record *pointer) {
 	return root;
 }
 
-size_t cut( size_t length ) {
+u_int cut( u_int length ) {
 	if (length % 2 == 0)
 		return length/2;
 	else
@@ -288,18 +291,18 @@ size_t cut( size_t length ) {
 }
 
 void reset_parent(Node *node){
-	for (size_t i = 0; i <= node->key_num; ++i)
+	for (u_int i = 0; i <= node->key_num; ++i)
 	{
 		((Node *)(node->pointers[i]))->parent = node;
 	}
 }
 
 // a full leaf, which need to split.
-Node *insert_into_leaf_and_split(Node *root, Node *leaf, char *key, size_t insertion_point, Record *record){
+Node *insert_into_leaf_and_split(Node *root, Node *leaf, char *key, u_int insertion_point, Record *record){
 	char *extra_key;
 	void *extra_pointer;
-	size_t old_rec_num;
-	size_t new_rec_num;
+	u_int old_rec_num;
+	u_int new_rec_num;
 	// move elements for insertion.
 	if (insertion_point < MAX_RECORDS_IN_LEAF){
 		extra_key     = leaf->keys[leaf->key_num - 1];
@@ -339,7 +342,7 @@ Node *inesrt_into_parent(Node *root, Node *left_node, char *key, Node *right_nod
 	}
 	right_node->parent = left_node->parent;
 	// get key index
-	size_t index = 0;
+	u_int index = 0;
 	while (index <= parent->key_num && parent->pointers[index] != left_node)
 		index++;
 	// happy case, engouh room for new node
@@ -355,11 +358,11 @@ Node *inesrt_into_parent(Node *root, Node *left_node, char *key, Node *right_nod
 }
 
 // a full ndoe which need to split
-Node *insert_into_node_and_split(Node *root, Node *old_node, char *key, size_t index, Node *left_node, Node *right_node){
+Node *insert_into_node_and_split(Node *root, Node *old_node, char *key, u_int index, Node *left_node, Node *right_node){
 	char *extra_key;
 	void *extra_pointer;
-	size_t old_node_key_num;
-	size_t new_node_key_num;
+	u_int old_node_key_num;
+	u_int new_node_key_num;
 	// move elements for insertion.
 	if (MAX_KEYS_IN_NODE > index){
 		extra_key = old_node->keys[old_node->key_num - 1];
@@ -393,13 +396,14 @@ void print_welcome(){
 void print_help(){
 	print_welcome();
 	printf( "h                      print this help\n"
-			"i <key><enter><value>  insert a record\n"
-			"d <key>                delete a record\n"
-			"q                      quit\n");
+			"i <key>%c<value>  insert a record\n"
+			"q                      quit\n"
+			"and file options:\n"
+			"-i data.txt\n", SEPATATOR);
 }
 
 void print_node(Node *node){
-	size_t i = 0;
+	u_int i = 0;
 	printf("|%p:[%u] ", node, node->key_num);
 	while (i < node->key_num){
 		printf("%p,", node->pointers[i]);
@@ -421,7 +425,7 @@ void print_layer(Node *left_node){
 
 void print_leaf(Node *leaf_node){
 	Record **record = (Record **)leaf_node->pointers;
-	size_t i = leaf_node->key_num;
+	u_int i = leaf_node->key_num;
 	printf("|%p:", leaf_node);
 	do{
 		printf(" %s,%s |", (*record)->key, (*record)->value);
@@ -475,15 +479,35 @@ FILE *ensure_open_file(char const *filename){
 	return file;
 }
 
+char *get_line(FILE *file){
+	char ch;
+	u_int buff_length = 16;
+	u_int line_length = 0; 
+	char *buff = (char *)malloc(buff_length);
+	while(EOF != (ch=fgetc(file))){
+		if (ch == '\n')
+			break;
+		buff[line_length] = ch;
+		line_length++;
+		if (line_length == buff_length - 1){
+			buff_length *= 2;
+			buff = realloc(buff, buff_length);
+		}
+	}
+	buff = realloc(buff, line_length + 1);
+	buff[line_length] = '\0';
+	return buff;
+}
+
 Record *parse_line(FILE *file, PARSE_LINE_ERROR_FLAG *error){
 	int buff_length = 16;
 	char *pointer = (char *)malloc(buff_length);
 	char *buff = pointer;
-	size_t line_length = 0;
+	u_int line_length = 0;
 	char ch;
 
 	bool comma_found = false;
-	size_t comma = 0; 		//buff[comma-1] == SEPARATER
+	u_int comma = 0; 		//buff[comma-1] == SEPATATOR
 	*error = SUCCESS;
 	while(*error == SUCCESS){
 		ch = fgetc(file);
@@ -505,8 +529,8 @@ Record *parse_line(FILE *file, PARSE_LINE_ERROR_FLAG *error){
 			default:
 				if (!comma_found){
 					comma++;
-					if (ch==SEPARATER){
-					ch = '\0';       //replace SEPARATER with '\0'
+					if (ch==SEPATATOR){
+					ch = '\0';       //replace SEPATATOR with '\0'
 					comma_found = true;
 					comma--;
 					}
@@ -542,7 +566,7 @@ Record *parse_line(FILE *file, PARSE_LINE_ERROR_FLAG *error){
 	}
 }
 
-Node *import_data(char const *filename, Node *root, size_t *error_count, size_t *record_count){
+Node *import_data(char const *filename, Node *root, u_int *error_count, u_int *record_count){
 	FILE *file = ensure_open_file(filename);
 	PARSE_LINE_ERROR_FLAG error = SUCCESS;
 	Record *record;
@@ -578,10 +602,29 @@ Node *import_data(char const *filename, Node *root, size_t *error_count, size_t 
 	return root;
 }
 
-void stdin_mode(int argc, char const *argv[]){
+int std_arg_parser(char *str, char **argv_p[]){
+	int argc = 1;
+	char **argv = (char **)malloc(6 * sizeof(char *));
+	*argv_p = argv;
+	*argv++;        // we don't have argv[0], the executable path.
+	*argv++ = str;
+	while (*str != '\0') {
+		if (*str == ' ') {
+			*str++ = '\0';
+			*argv++ = str;
+			argc++;
+			while (*str == ' ') str++;
+		}
+		str++;
+	}
+
+	return argc;
+}
+
+Node *stdin_mode(int argc, char const *argv[]){
 	clock_t tImportStart;
-	size_t error_count = 0;
-	size_t record_count= 0;
+	u_int error_count = 0;
+	u_int record_count= 0;
 	Node *root = NULL;
 	if (*argv[1] != '-')
 		fatal("need a option");
@@ -604,7 +647,7 @@ void stdin_mode(int argc, char const *argv[]){
 			root = import_data(argv[2], root, &error_count, &record_count);
 			printf("Note: %u records imported within %.2fs with %d parse error.\n", record_count,
 							 (double)(clock() - tImportStart)/CLOCKS_PER_SEC, error_count);
-			interactive_mode(root);
+			// interactive_mode(root);
 			break;
 		case 's':
 			if (argc < 2)
@@ -622,6 +665,7 @@ void stdin_mode(int argc, char const *argv[]){
 			//delete_data(argv[2]);
 			break;
 	}
+	return root;
 }
 
 void interactive_mode(Node *root){
@@ -629,9 +673,8 @@ void interactive_mode(Node *root){
 	PARSE_LINE_ERROR_FLAG error = SUCCESS;
 	Record *record;
 	clock_t tStart;
-	print_welcome();
 	printf("> ");
-	while (scanf("%c", &command) != EOF) {
+	while (EOF!= (command = fgetc(stdin))) {
 		// printf("into while\n");
 		switch (command) {
 			case 'h':
@@ -645,55 +688,77 @@ void interactive_mode(Node *root){
 				record = parse_line(stdin, &error);
 				if (error!=SUCCESS){
 					printf("parse error\n"
-						"key and value should be separated by comma. for example:\n"
-						"name,passwordhash\n");
+						"key and value should be separated by a SEPATATOR. for example:\n"
+						"name%cpasswordhash\n", SEPATATOR);
 				}else{
 					tStart = clock();
-					insert_record(root, record);
+					root = insert_record(root, record);
 					print_record(record);
 					printf("inserted within %.2fs.\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 				}
 				break;
-			case 's':
-				fgetc(stdin);   //jump a space
-				record = parse_line(stdin, &error);
-				if (error!=SUCCESS){
-					printf("type key to select end with a comma. for example:\n"
-							"Turing,\n");
+			case 's':{
+				if (!root){
+					printf("need import some records first\n");
 				}else{
 					tStart = clock();
-					record = find(root, record->key);
+					fgetc(stdin);   //jump a space
+					char *key = get_line(stdin);
+					record = find(root, key);
+					free(key);
 					if (!record) {
 						printf("Not found\n");
 					}else{
 						print_record(record);
 					}
 					printf("Completed within %.6fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+					ungetc('\n', stdin);
 				}
 				break;
+			}
 			case 'p':
 				print_tree(root);
 				break;
+			case '-':{  // parse command and jump to stdin
+				int i = 0;
+				char buff[1024];
+				int argc;
+				char **argv;
+				ungetc('-', stdin);
+				while ('\n'!=(command=fgetc(stdin))){
+					buff[i] = command;
+					i++;
+					if (i == 1024) break;
+				}
+				buff[i] = '\0';
+				argc = std_arg_parser(buff, &argv);
+				root = stdin_mode(argc, argv);
+				free(argv);
+				ungetc('\n', stdin);
+				break;
+			}
 			case '\n':
+				printf("> ");
 				break;
 			default:
 				printf("unknown command:%c\n", command);
 				break;
 		}
 	// while (fgetc(stdin)!='\n')
-		// ;
-	printf("> ");
+	// 	;
+	
+
 	}
 }
 
 int main(int argc, char const *argv[])
 {
 	Node *root = NULL;
+	print_welcome();
 	if (argc > 1)
 	{
-		stdin_mode(argc, argv);
-	}else{
-		interactive_mode(root);
+		root = stdin_mode(argc, argv);
 	}
+	interactive_mode(root);
 	return 0;
 }
